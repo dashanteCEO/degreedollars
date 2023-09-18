69943d9e0848e47f9e93835ddd640f98533676f4
dashanteCEO <deshantestreet749@gmail.com>
2023-09-18 18:52:24 -0500

    Completed utility to generate env sample

--- utils/generateEnvSample.ts
+++ utils/generateEnvSample.ts
@@ -20,17 +20,23 @@ const generateEnvSample = async () => {
   // STEP 1: READ THE MAIN DIRECTORY
   const main_directory = await fs.readdir(MAIN_DIR, "utf-8");
   const hasEnvFile = main_directory.includes(".env");
+  if (!hasEnvFile) return console.log("NO ENV FILE FOUND");
   // STEP 2: READ THE ENV FILE IF PRESENT
-  let envFile: string;
-  if (!hasEnvFile) return console.log("NO ENV FILE DETECTED!");
-  envFile = await fs.readFile(`${MAIN_DIR}/.env`, "utf-8");
+  let envFile = await fs.readFile(`${MAIN_DIR}/.env`, "utf-8");
   // STEP 3: READ THE SAMPLE ENV FILE IF PRESENT
-  const hasSampleEnv = main_directory.includes(`${MAIN_DIR}/.env.sample`);
+  const hasSampleEnv = main_directory.includes(`.env.sample`);
   let sampleEnv: string;
-  sampleEnv = await fs.readFile(`${MAIN_DIR}/.env`, "utf-8");
-  console.log(generateSampleEnvContent(sampleEnv));
   if (hasSampleEnv) {
+    sampleEnv = await fs.readFile(`${MAIN_DIR}/.env.sample`, "utf-8");
+    const sampleContent = generateSampleEnvContent(envFile);
+    if (sampleContent === sampleEnv)
+      return console.log("NO BLUEPRINT GENERATED. CONTENT WAS THE SAME");
     // We basically generate the markup for the file. if it matches the existing sample, then we return
+  } else {
+    const sampleContent = generateSampleEnvContent(envFile);
+    await fs.writeFile(`${MAIN_DIR}/.env.sample`, sampleContent, {
+      encoding: "utf-8",
+    });
   }
   // STEP 4: EXTRACT THE KEYS FROM THE ENV FILE
   // STEP 5: FORMAT THE STRING RETURNED INTO: \nKEY=
