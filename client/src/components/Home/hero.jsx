a8da0c07ddad45fa880950c7bb6a13ee03e32b1e
dashanteCEO <deshantestreet749@gmail.com>
2023-10-24 15:22:39 -0500

    force push

--- client/src/components/Home/hero.jsx
+++ client/src/components/Home/hero.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import slugify from "slugify";
+import { MdFilterList } from 'react-icons/md'
 
 function Hero() {
-
   return (
     <div className="p-3 flex justify-evenly w-full items-center 
     flex-col h-[400px] mt-8 pt-4">
-      <h2 className='text-6xl' >
+      <h2 className='text-6xl font-medium '>
         Elevate Dreams, Fund Futures
       </h2>
       <div className='flex flex-col items-center h-[60%] justify-center'>
@@ -19,12 +19,10 @@ function Hero() {
         text-[#333333]" placeholder="Search..."/>
       <button className="flex items-center justify-center w-[20%] h-full
         bg-primary rounded-xl hover:duration-500 hover:bg-[#12c77c] transition-all">
-        <h3 className="text-white text-[18px] font-medium">
-          Icon
-        </h3>
+        <MdFilterList className="text-white text-[25px] ml-2"/>
       </button>
       </div>
-      <ul className="flex justify-between items-center w-[60%]  ">
+      <div className="flex justify-between items-center w-[60%]  ">
         <ul className="flex justify-between items-center w-[77.3%]  rounded-xl  ">
           {["campaign", "school", "supplies","student living", "tuition"].map((link) => {
             return (
@@ -37,7 +35,7 @@ function Hero() {
             );
           })}
         </ul>
-      </ul>
+      </div>
     </div>
   );
 }
