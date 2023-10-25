d34df8e354189a3ace8a0174231a4b11f98285e4
dashanteCEO <deshantestreet749@gmail.com>
2023-10-25 17:38:31 -0500

    hero for now

--- client/src/components/Home/hero.jsx
+++ client/src/components/Home/hero.jsx
@@ -3,6 +3,9 @@ import slugify from "slugify";
 import { MdFilterList } from 'react-icons/md'
 
 function Hero() {
+
+  const [ placeholder, setPlaceholder ] = useState('Search...')
+
   return (
     <div className="p-3 flex justify-evenly w-full items-center 
     flex-col h-[400px] mt-8 pt-4">
@@ -10,26 +13,26 @@ function Hero() {
         Elevate Dreams, Fund Futures
       </h2>
       <div className='flex flex-col items-center h-[60%] justify-center'>
-        <h3 className='text-xl flex items-center text-normal'>Education is the key to unloacking a brighter future, and every contribution counts.</h3>
-        <h3 className='text-xl flex items-center text-normal'>Begin by searching through the various categories below.</h3>
+        <h3 className='text-xl flex items-center text-normal'>Education is the key to unlocking a brighter future, with your help someone education journey will be possible.</h3>
+        <h3 className='text-xl flex items-center text-normal'> Take a look at our website to see how you can help make a dream a reality.</h3>
       </div>
       <div className="items-center w-[60%] rounded-xl flex-row flex justify-between h-16 ">        
       <input type="text" className="w-[78%] h-full bg-[#f5f5f5] 
         rounded-xl outline-none pl-5 text-[20px] font-normal 
-        text-[#333333]" placeholder="Search..."/>
+        text-[#333333]" placeholder={placeholder}/>
       <button className="flex items-center justify-center w-[20%] h-full
         bg-primary rounded-xl hover:duration-500 hover:bg-[#12c77c] transition-all">
         <MdFilterList className="text-white text-[25px] ml-2"/>
       </button>
       </div>
-      <div className="flex justify-between items-center w-[60%]  ">
-        <ul className="flex justify-between items-center w-[77.3%]  rounded-xl  ">
+      <div className="flex justify-between items-center w-[60%]">
+        <ul className="flex justify-between items-center w-[78%] rounded-xl  ">
           {["campaign", "school", "supplies","student living", "tuition"].map((link) => {
             return (
               <li key={link} className='p-2 px-4 border-2 border-[#f5f5f5] bg-[#f5f5f5] rounded-xl h-full
               items-center justify-center flex hover:bg-white mt-2
               hover:duration-500 cursor-pointer hover:text-primary hover:border-primary
-              capitalize text-black font-medium'>
+              capitalize text-black font-medium' onClick={()=> setPlaceholder("Search " + link)} >
               {link}
               </li>
             );
