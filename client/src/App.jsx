49dc8c94c88fec3660b20f0eb928cd0ba3ddb1e0
dashanteCEO <deshantestreet749@gmail.com>
2023-10-28 00:56:47 -0500

    slider without animations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Login from "./components/Auth/login";
 import Home from "./components/Home/home";
 import Header from "./components/header";
 import PageNotFound from "./components/pageNotFound";
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
           <Route path='*' element={<PageNotFound/>} />
         </Routes>
       </Router>
