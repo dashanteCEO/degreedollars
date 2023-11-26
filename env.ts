4e08e2bc99267047e2eb68e2aa249813d209f1b4
dashanteCEO <deshantestreet749@gmail.com>
2023-11-26 00:06:21 -0500

    n

--- env.ts
+++
@@ -1,8 +0,0 @@
-import { cleanEnv, port, str } from "envalid";
-
-export default cleanEnv(process.env, {
-  NODE_ENV: str({ choices: ["development", "production"] }),
-  MONGO: str(),
-  MONGO_PASSWORD: str({ default: "" }),
-  PORT: port({ default: 3000 }),
-});
